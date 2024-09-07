f65b03152d9f165ffe3bd71e9c0801eeb8533310
jacoobanderson <j00cwa@gmail.com>
2024-09-07 16:47:02 +0200

    Create initial configuration

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	TargetURL string `json:"target_url"`
+	Port      string `json:"port"`
+}
+
+func LoadConfig(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var config Config
+	decoder := json.NewDecoder(file)
+
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
