5610f218d783b4148e334aa592cf806923040f1c
jacoobanderson <j00cwa@gmail.com>
2024-09-07 18:06:10 +0200

    Add health endpoints to testing servers

--- cmd/server2/main.go
+++ cmd/server2/main.go
@@ -11,8 +11,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Request received")
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This server is healthy")
+	w.WriteHeader(http.StatusOK)
+	log.Printf("Healthy!!")
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/health", healthHandler)
 	port := ":8083"
 	log.Printf("Target server0 listening on port %s", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
